55246503f122715f39a75484780ef52c1d9d4e9e
jakobpederson <jakob.pederson+github@gmail.com>
2017-06-17 10:45:19 -0500

    initial commit

---
+++ migratory_birds.py
@@ -0,0 +1,35 @@
+from collections import Counter
+
+class InvalidData(Exception):
+    pass
+
+
+class BirdCounter():
+
+    def migratory_birds(self, n, ar):
+        error_check = self.validate_data(n, ar)
+        if error_check == 'validated':
+            count = dict(Counter(ar))
+            max_bird_number = max([x for x in count.values()])
+            lowest_type = min([x for x, j in count.items() if j == max_bird_number])
+            return lowest_type
+        return 'invalid data'
+
+    def validate_data(self, n, ar):
+        try:
+            self.checks(n, ar)
+            return 'validated'
+        except InvalidData as e:
+            print(e)
+            return e
+
+    def checks(self, n, ar):
+        if len(ar) > 0:
+            ar_check = [x for x in ar if x <= 0 or x > 5]
+            print(ar)
+            if n <= 0 or n > 1e15:
+                raise InvalidData('Invalid integer n')
+            elif len(ar_check) > 0:
+                raise InvalidData('Invalid type in array arr')
+        else:
+            raise InvalidData('No values in ar')
---
+++ test_migratory_birds.py
@@ -0,0 +1,33 @@
+import unittest
+import migratory_birds
+
+
+class TestBirdCounter(unittest.TestCase):
+
+    def setUp(self):
+        self.client = migratory_birds.BirdCounter()
+
+    def test_migratory_birds(self):
+        self.assertEqual(2, self.client.migratory_birds(2, [2, 2, 2, 2, 3]))
+        self.assertEqual(3, self.client.migratory_birds(3, [2, 2, 2, 2, 3, 3, 3, 3, 3]))
+        self.assertEqual(4, self.client.migratory_birds(4, [2, 2, 2, 2, 3, 4, 4, 4, 4, 4]))
+        self.assertEqual(5, self.client.migratory_birds(5, [2, 2, 2, 2, 3, 5, 5, 5, 5, 5]))
+
+    def test_migratory_birds_always_return_lowest_answer(self):
+        self.assertEqual(2, self.client.migratory_birds(2, [2, 2, 2, 2, 2, 3, 3, 3, 3, 3]))
+        self.assertEqual(3, self.client.migratory_birds(3, [4, 4, 4, 4, 4, 3, 3, 3, 3, 3]))
+        self.assertEqual(4, self.client.migratory_birds(4, [7, 7, 7, 7, 7, 3, 4, 4, 4, 4, 4]))
+        self.assertEqual(5, self.client.migratory_birds(5, [6, 6, 6, 6, 6, 5, 5, 5, 5, 5]))
+
+    def test_validate_data(self):
+        self.fail(self.client.validate_data(0, [2, 2, 2, 3]))
+
+    def test_validate_data_in_migratory_birds(self):
+        self.assertEqual('invalid data', self.client.migratory_birds(0, [2, 2, 2, 2, 2, 3, 3, 3, 3, 3]))
+        self.assertEqual('invalid data', self.client.migratory_birds(-1, [2, 2, 2, 2, 2, 3, 3, 3, 3, 3]))
+        self.assertEqual('invalid data', self.client.migratory_birds(3e15, [2, 2, 2, 2, 2, 3, 3, 3, 3, 3]))
+        self.assertEqual('invalid data', self.client.migratory_birds(2, []))
+        self.assertEqual('invalid data', self.client.migratory_birds(2, [7, 7, 7]))
+        self.assertEqual('invalid data', self.client.migratory_birds(2, [0, 0, 2]))
+
+
